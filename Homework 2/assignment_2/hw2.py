4eecb7f78f8bc11ec23f7d89c635d7de5d67112e
Vishal <vishl.sriram@gmail.com>
2018-10-14 22:31:40 -0500

    Update Homework 2/assignment_2/hw2.py

--- Homework 2/assignment_2/hw2.py
+++ Homework 2/assignment_2/hw2.py
@@ -1,13 +1,21 @@
 import requests
 import re
 import matplotlib.pyplot as plt
-
+import json
 
 def lyrics_word_count_easy(artist, song, phrase):
-    pass
+	response = requests.get("https://api.lyrics.ovh/v1/" + artist + "/" + song)
+	if response.status_code != 200:
+		print(response.status_code)
+		return -1
+	json = response.json()
+	n = re.findall(phrase, json["lyrics"], re.IGNORECASE)
+	return len(n)
 
 def lyrics_word_count(artist, phrase):
-    pass
+    response = requests.get("http://api.musicgraph.com/api/v2/artist/suggest?api_key=c8303e90962e3a5ebd5a1f260a69b138&prefix=gree&limit=3")
+    return response.status_code
 
+print(lyrics_word_count("thihng", "thing"))
 def visualize():
     pass
